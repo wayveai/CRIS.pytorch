8ac66e7fb62b4f23f9cb687c65b1ad2e1f0e7728
Thomas Newton <tomnewton@wayve.ai>
2022-09-13 00:24:09 +0000

    fixes to setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 python3.8 -m venv env
 source env/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirement.txt
 mkdir pretrain
 cd pretrain
 wget https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt
 wget https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt
+cd ..
